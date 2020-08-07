3bf8c4e4e82e0a874ad7863c73bbb48b7d9b81c9
koy1619 <maxiaolei007@sina.com>
2020-08-07 11:59:37 +0800

    update yaml/nginx-ingress.yaml:smiling_imp::ok::ok_hand:

--- upload.sh
+++ upload.sh
@@ -7,5 +7,5 @@ echo "请输入本次提交的说明:"
 read description
 git add -A
 git commit -m "$description:smiling_imp::ok::ok_hand:" .
-git remote add origin git@github.com:koy1619/k8s-v1.18.2-binary-install.git
+git remote add origin git@github.com:koy1619/k8s-normal.git
 git push -u origin master
