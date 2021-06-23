2a9affe489985e8a73c1ced2699255653711fbbe
koy1619 <maxiaolei007@sina.com>
2021-06-23 14:00:16 +0800

    update:smiling_imp::ok::ok_hand:

--- shell/init.sh
+++ shell/init.sh
@@ -145,6 +145,7 @@ function Install_docker(){
     yum -y install docker-ce-19.03.6 docker-ce-cli-19.03.6
 
 ##centos8 Install_docker####
+    sudo yum install https://download.docker.com/linux/fedora/30/x86_64/stable/Packages/containerd.io-1.2.13-3.2.fc30.x86_64.rpm
     sudo yum install -y yum-utils  device-mapper-persistent-data  lvm2
     sudo yum-config-manager  --add-repo   https://download.docker.com/linux/centos/docker-ce.repo
     sudo yum install docker-ce docker-ce-cli containerd.io
